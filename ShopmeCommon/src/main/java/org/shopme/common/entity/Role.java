2d0d6d22a68ab521b731c32227f2babe98f8bf14
Ismat Aghamirzayev <ismataga1@gmail.com>
2024-07-29 08:04:50 +0400

    adds front menu bar

---
+++ ShopmeCommon/src/main/java/org/shopme/common/entity/Role.java
@@ -0,0 +1,23 @@
+package org.shopme.common.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(length=40, nullable = false)
+    private String name;
+    private String description;
+
+}
